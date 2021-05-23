b8ad209b3931fe22643e142fcff0819246b753f6
Vikas Kumar <negi.vikash925@gmail.com>
2021-05-23 18:47:32 +0530

    Fix issue

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -8,19 +8,19 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time
-    start_time = time.localtime()
+    start_time = time.time()
 
 
 @hooks.register("written")
 def written():
     global total_written
-    total_written += 1
+    total_written = total_written + 1
 
 
 @hooks.register("stats")
 def stats():
-    final_time = time.localtime() - start_time
-    average = final_time / total_written
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
     report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
     print(report.format(total_written, final_time, average))
 
