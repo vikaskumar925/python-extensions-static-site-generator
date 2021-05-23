6c0e1d1594965e76b17a956ca3aac55298671c77
Vikas Kumar <negi.vikash925@gmail.com>
2021-05-23 18:06:19 +0530

    Fix typo

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,22 +1,22 @@
-_callback = {}
+_callbacks = {}
 
 def register(hook, order=0):
     def register_callback(func):
-        _callback.setdefault(hook, {}).setdefault(order, []).append(func)
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append(func)
         return func
 
     return register_callback
 
 
 def event(hook, *args):
-    for order in sorted(_callback.get(hook, {})):
-        for func in _callback[hook][order]:
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
             func(*args)
 
 
 def filter(hook, value, *args):
-    for order in sorted(_callback.get(hook, {})):
-        for func in _callback[hook][order]:
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
             value = func(value, *args)
     return value
 
